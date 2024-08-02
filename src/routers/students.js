5cb7c21e4b97a1efc8b33a458cb57d0efee8f3e6
KiraKostina <kostina.kira@gmail.com>
2024-08-03 00:16:04 +0300

    refresh, authenticate

--- src/routers/students.js
+++ src/routers/students.js
@@ -12,8 +12,11 @@ import { validateBody } from '../middlewares/validateBody.js';
 import { createStudentSchema } from '../validation/students.js';
 import { updateStudentSchema } from '../validation/students.js';
 import { isValidId } from '../middlewares/isValidId.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
+
+router.use(authenticate);
 
 router.get('/', ctrlWrapper(getStudentsController));
 
