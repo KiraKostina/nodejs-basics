44de87f7ede35cfe205fc9debff178a7b396a788
KiraKostina <kostina.kira@gmail.com>
2024-08-09 23:20:15 +0300

    request-reset-email

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,5 @@
+import path from 'node:path';
+
 export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
@@ -10,3 +12,19 @@ export const ROLES = {
   TEACHER: 'teacher',
   PARENT: 'parent',
 };
+
+export const SMTP = {
+  SMTP_HOST: 'SMTP_HOST',
+  SMTP_PORT: 'SMTP_PORT',
+  SMTP_USER: 'SMTP_USER',
+  SMTP_PASSWORD: 'SMTP_PASSWORD',
+  SMTP_FROM: 'SMTP_FROM',
+
+  // SMTP_HOST: process.env.SMTP_HOST,
+  // SMTP_PORT: Number(process.env.SMTP_PORT),
+  // SMTP_USER: process.env.SMTP_USER,
+  // SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+  // SMTP_FROM_EMAIL: process.env.SMTP_FROM_EMAIL,
+};
+
+export const TEMPLATES_DIR = path.join(process.cwd(), 'src', 'templates');
