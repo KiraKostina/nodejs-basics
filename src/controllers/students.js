9c85eac3b404306853224880c1c09cc915aee754
KiraKostina <kostina.kira@gmail.com>
2024-08-11 00:32:24 +0300

    Update students.js

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -12,6 +12,8 @@ import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 import { parseSortParams } from '../utils/parseSortParams.js';
 import { parseFilterParams } from '../utils/parseFilterParams.js';
 import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { env } from '../utils/env.js';
 
 export const getStudentsController = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
@@ -89,13 +91,17 @@ export const upsertStudentController = async (req, res) => {
   });
 };
 
-export const patchStudentController = async (req, res) => {
+export const patchStudentController = async (req, res, next) => {
   const { studentId } = req.params;
   const photo = req.file;
   let photoUrl;
 
   if (photo) {
-    photoUrl = await saveFileToUploadDir(photo);
+    if (env('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
   }
 
   const result = await updateStudent(studentId, {
